db835b376abc764c669f7a8e1010c35fa3fe70b2
empipio <94937507+empipio@users.noreply.github.com>
2022-01-08 21:15:55 +0000

    pseudocode added to js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
+
+//button to click
+//event listener to trigger game once clicked
+//timer starts (set time interval)
+//question pops up (ordered list, boxes to click)
+//user answers question (event listener on parent and if/else to determine behaviour according to whether right or wrong)
+    //if right, user told and points added to score
+    //if wrong, user told and time subtracted from clock
+//answering of question triggers presentation of another question
+//all qs answered/out of time = end of game
+//save initials and score to local storage (input field for name, transfer score)
+
